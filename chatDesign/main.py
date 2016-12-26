bbacdcdb7a0ff8d268f113b0f39d51f5bc8b5ef9
Dingchao Zhang <dingchaoie@gmail.com>
2016-12-25 18:05:58 -0600

    testing different css and js functions to improve the beauty of ui

--- chatDesign/main.py
+++ chatDesign/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('chat.html')
 
 @app.route("/ask", methods=['POST'])
 def ask():
